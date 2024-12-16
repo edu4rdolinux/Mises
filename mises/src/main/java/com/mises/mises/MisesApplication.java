ce73c0d7b619101de7c457a5cc40dfde6f623e86
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-16 00:33:17 -0300

    Nest to Spring

---
+++ mises/src/main/java/com/mises/mises/MisesApplication.java
@@ -0,0 +1,13 @@
+package com.mises.mises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisesApplication.class, args);
+	}
+
+}
---
+++ mises/src/test/java/com/mises/mises/MisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mises.mises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
