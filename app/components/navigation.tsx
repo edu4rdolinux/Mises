a23f46c990c3dd4cd3cb2b325f850025455e46dd
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-16 15:35:36 -0300

    Quem Somos

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -3,6 +3,7 @@ import NavigationOption from "./navigationOption"
 const Navigation = () => {
     return (
         <div className="flex gap-8 mr-8">
+            <NavigationOption Option="Quem" AdditionalText="Somos"/>
             <NavigationOption Option="Artigos"/>
             <NavigationOption Option="Academia" AdditionalText="Mises"/>
             <NavigationOption Option="Livraria"/>
