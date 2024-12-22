ecde296d63c814ab13253de2f65fd3cf1e9bf093
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-22 13:45:25 -0300

    Spacing

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -3,11 +3,11 @@ import NavigationOption from "./navigationOption"
 const Navigation = () => {
     return (
         <div className="flex gap-44">
-            <NavigationOption Option="Quem" AdditionalText="Somos"/>
+            <span className="ml-8"><NavigationOption Option="Quem" AdditionalText="Somos"/></span>
             <NavigationOption Option="Artigos"/>
             <NavigationOption Option="Academia" AdditionalText="Mises"/>
             <NavigationOption Option="Livraria"/>
-            <NavigationOption Option="Loja"/>
+            <span className="mr-8"><NavigationOption Option="Loja"/></span>
         </div>
     )
 }
